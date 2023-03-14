5ccdbfe83dc7bd81281d902fcde8ecb74ffdd020
Dan{ } <123217422+DanSSV@users.noreply.github.com>
2023-03-14 12:26:20 +0800

    Add files via upload

--- sales.php
+++ sales.php
@@ -28,7 +28,7 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="sales.html">Home</a>
+            <a class="nav-link active" aria-current="page" href="sales.php">Home</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">Products</a>
