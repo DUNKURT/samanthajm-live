0b904fceabec01242201eb4016d6f449e7e6b7aa
Dan{ } <123217422+DanSSV@users.noreply.github.com>
2023-03-16 09:39:18 +0800

    Add files via upload

--- sales.php
+++ sales.php
@@ -9,7 +9,7 @@
     integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous" />
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
   <link rel="stylesheet" href="css/sales.css" />
-  <title>Samantha | Sales</title>
+  <title>Samantha | Dashboard</title>
   <?php
   $imagePath = "image/SJM_Icon.png";
   ?>
